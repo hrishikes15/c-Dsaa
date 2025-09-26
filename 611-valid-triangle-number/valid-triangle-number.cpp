4bd3b3b341f4b47bd62085837385977b129b17f2
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-09-26 10:30:33 +0530

    Time: 27 ms (89.51%) | Memory: 16.7 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());  
         int n = nums.size();
-        if( n < 3 ) return 0;
-        sort(nums.begin(),nums.end());
         int count = 0 ;
         for(int k = n-1 ; k >=2 ; k--){
-            int low = 0 , high = k-1;
-            while( low <= high){
-                if( nums[low] + nums[high] > nums[k]){
-                    count += ( high - low );
+            int low = 0 ;
+            int high = k-1;
+            while(low < high){
+                if(nums[low] + nums[high] > nums[k]){
+                    count += high - low ;
                     high--;
                 }
                 else{
                     low++;
                 }
             }
+
+            
         }
         return count;
     }
