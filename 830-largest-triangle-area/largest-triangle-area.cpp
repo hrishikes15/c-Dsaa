305bab99a8ae2067df3709cb117085dd7a21161e
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-09-27 18:07:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (63.36%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    double largestTriangleArea(vector<vector<int>>& points) {
-        int n = points.size();
-        
-        double area = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                for(int k = 0 ; k < n ; k++){
-                    int x1 = points[i][0] , y1 = points[i][1];
-                    int x2 = points[j][0] , y2 = points[j][1];
-                    int x3 = points[k][0] , y3 = points[k][1];
+    double largestTriangleArea(vector<vector<int>>&p) {
+        double res = 0;
 
-                    double curr_area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1-y2));
-                    area = max(area , curr_area);
+        int n = p.size();
+        for(int i=0; i<n-2; i++)
+            for(int j=i+1; j<n-1; j++)
+                for(int k=j+1; k<n; k++)
+                {
+                    double area = abs( (p[j][0]-p[i][0]) * (p[k][1]-p[i][1]) - (p[j][1] - p[i][1]) * (p[k][0] - p[i][0]) ) * 0.5;
+                    res = max(res, area);
                 }
-            }
-        }
-        return area;
+        return res;
     }
 };
