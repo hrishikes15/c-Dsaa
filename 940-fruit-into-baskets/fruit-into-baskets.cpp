1a01ff6fc1b7bd6ed41aa679b7bc9b54c37e980a
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-09-28 19:39:04 +0530

    Time: 35 ms (67.97%) | Memory: 75.2 MB (18.88%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int totalFruit(vector<int>& arr) {
         unordered_map<int,int> mapp;
-        int i = 0, j = 0;
+        int i = 0 , j = 0 ;
         int n = arr.size();
-        int maxi = 0;
+        int maxi = 0 ;
 
-        while (j < n) {
+        while( j < n ){
             mapp[arr[j]]++;
 
-            // If more than 2 fruits, shrink window
-            while (mapp.size() > 2) {
-                mapp[arr[i]]--;
-                if (mapp[arr[i]] == 0) {
-                    mapp.erase(arr[i]);
+            if(mapp.size() > 2){
+                while(mapp.size() > 2){
+                    mapp[arr[i]]--;
+                    if(mapp[arr[i]] ==0 ){
+                        mapp.erase(arr[i]);
+                    }
+                    i++;
                 }
-                i++;
-            }
 
-            // Update maximum after adjusting window
-            maxi = max(maxi, j - i + 1);
+            }
+            maxi = max(maxi , (j-i+1));
             j++;
         }
-
         return maxi;
     }
 };
