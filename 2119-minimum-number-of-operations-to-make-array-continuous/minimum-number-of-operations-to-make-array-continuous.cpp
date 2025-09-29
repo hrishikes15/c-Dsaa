37089ebe65785e99b19720055e1db6640b750a6c
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-09-29 22:02:34 +0530

    Time: 40 ms (80.29%) | Memory: 68.1 MB (77.72%) - LeetSync

--- 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int original_n = nums.size();
-
         sort(nums.begin(), nums.end());
-        nums.erase(unique(nums.begin(), nums.end()), nums.end());
-
-        int n = nums.size();
         int i = 0, j = 0;
-        int maxWindow = 0;
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        int n = nums.size();
+        int maxLen = 0;
 
         while (i < n) {
-            while (j < n && nums[j] - nums[i] < original_n) {     
+            while (j < n && nums[j] - nums[i] < original_n) {
                 j++;
             }
-            maxWindow = max(maxWindow, j - i  );
+            maxLen = max(maxLen, (j - i));
             i++;
         }
-
-        return original_n - maxWindow;
+        return original_n - maxLen;
     }
 };
