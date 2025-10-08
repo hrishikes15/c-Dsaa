1503831f3aa44ca2aaf85905d983d5262338e9b3
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-10-08 18:59:29 +0530

    Time: 38 ms (59.13%) | Memory: 32.3 MB (37.69%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -33,25 +33,3 @@ public:
 
 
 
-const size_t BUFFER_SIZE = 0x6fafffff;
-alignas(std::max_align_t) char buffer[BUFFER_SIZE];
-size_t buffer_pos = 0;
-void* operator new(size_t size) {
-	constexpr std::size_t alignment = alignof(std::max_align_t);
-	size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
-	size_t total_size = size + padding;
-	char* aligned_ptr = &buffer[buffer_pos + padding];
-	buffer_pos += total_size;
-	return aligned_ptr;
-}
-void operator delete(void* ptr, unsigned long) {}
-void operator delete(void* ptr) {}
-void operator delete[](void* ptr) {}
-
-const auto __ = []() {
-    struct Leetcode {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&Leetcode::_);
-    return 0;
-}();
