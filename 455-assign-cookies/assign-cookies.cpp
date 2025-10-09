77bac4889de81b87eb3a69d6034099f2cb5856cb
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-10-09 17:30:05 +0530

    Time: 7 ms (73.48%) | Memory: 44.9 MB (43.38%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -4,19 +4,19 @@ public:
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int count = 0;
-        int i = 0;
-        int j = 0;
+        int l = 0;
+        int r = 0;
 
         int n = g.size();
         int m = s.size();
-        while(i < n && j < m){
-            if(g[i] <= s[j]){
+        while(r < n && l < m){
+            if(g[r] <= s[l]){
                 count++;
-                i++;
-                j++;
+                l++;
+                r++;
             }
             else{
-                j++;
+                l++;
             }
 
         }
