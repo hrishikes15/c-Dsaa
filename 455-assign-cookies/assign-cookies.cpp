dba515a94e5f3b050e260834f195219228b3a2c3
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-10-09 17:20:38 +0530

    Time: 5 ms (82.53%) | Memory: 44.6 MB (95.79%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        int n = g.size();
+        int m = s.size();
+        while(i < n && j < m){
+            if(g[i] <= s[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+
+        }
+        return count;
+    }
+};
