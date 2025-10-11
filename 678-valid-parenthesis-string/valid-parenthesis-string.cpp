4fc99428c0be3d9299b12dcd86401a819e150ec8
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2025-10-11 19:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.60%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.size();
+        int mini = 0 ;
+        int maxi = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(s[i] == '('){
+                mini++;
+                maxi++;
+            }
+            else if(s[i] == ')'){
+                mini--;
+                maxi--;
+            }
+            else{
+                mini--;
+                maxi++;
+            }
+            if(mini < 0){
+                    mini = 0;
+                }
+
+            if(maxi < 0){
+                return false;
+            }
+
+
+        }
+        return (mini == 0);
+
+    }
+};
