12834b06fb62d9e30f70f7e6e1b6a1df00fff205
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-01-21 14:56:36 +0530

    Time: 31 ms (11.37%) | Memory: 44.8 MB (5.68%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,57 +1,53 @@
 class Solution {
-    vector<vector<int>> lcsTable(string s1 , string s2 , int m , int n){
-        vector<vector<int>> dp(m+1 , vector<int>(n+1,0));
+    vector<vector<int>> lcsTable(string s1, string s2, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for(int i = 1 ; i <= m ; i++){
+        for (int i = 1; i <= m; i++) {
 
-            for(int j = 1 ; j <= n ; j++){
+            for (int j = 1; j <= n; j++) {
 
-                if(s1[i-1] == s2[j-1]){
+                if (s1[i - 1] == s2[j - 1]) {
 
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
 
-                else{
+                else {
 
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
-
             }
         }
 
         return dp;
     }
-    string printScs(vector<vector<int>> dp , string s1 , string s2 , int m , int n){
-        int i = m ;
+    string printScs(vector<vector<int>> dp, string s1, string s2, int m,
+                    int n) {
+        int i = m;
         int j = n;
         string ans;
 
-        while(i > 0 && j > 0){
-            if(s1[i-1] == s2[j-1]){
-                ans.push_back(s1[i-1]);
-                i--;j--;
-            }
-            else{
-                if(dp[i-1][j] > dp[i][j-1]){
-                    ans.push_back(s1[i-1]);
-                    i--;
-                }
-                else{
-                    ans.push_back(s2[j-1]);
-                    j--;
-                }
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                ans.push_back(s1[i - 1]);
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                ans.push_back(s1[i - 1]);
+                i--;
+            } else {
+                ans.push_back(s2[j - 1]);
+                j--;
             }
         }
-        while(i > 0){
-            ans.push_back(s1[i-1]);
+        while (i > 0) {
+            ans.push_back(s1[i - 1]);
             i--;
         }
-        while(j > 0){
-            ans.push_back(s2[j-1]);
+        while (j > 0) {
+            ans.push_back(s2[j - 1]);
             j--;
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 
@@ -60,9 +56,8 @@ public:
         int m = str1.size();
         int n = str2.size();
 
-        vector<vector<int>> table = lcsTable(str1,str2,m,n);
+        vector<vector<int>> table = lcsTable(str1, str2, m, n);
 
-        return printScs(table , str1 , str2 , m , n);
-
+        return printScs(table, str1, str2, m, n);
     }
 };
