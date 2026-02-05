57145b76cdaa90454fb1c6834b400c431b3a042d
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-05 21:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (76.69%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -5,15 +5,6 @@ public:
         string s2;
         for(int i = 0 ; i < word1.size() ; i++) s1+=word1[i];
         for(int i = 0 ; i < word2.size() ; i++) s2+=word2[i];
-
-        int n = s1.size() ;int m = s2.size();
-        if(n != m) return false;
-
-        int i = 0 , j = 0;
-        while(i < n && j < m){
-            if(s1[i] != s2[j]) return false;
-            i++;j++;
-        }
-        return true;
+        return s1==s2;
     }
 };
