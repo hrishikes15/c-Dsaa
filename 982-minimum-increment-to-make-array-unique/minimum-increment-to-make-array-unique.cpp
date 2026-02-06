37b3c29da27affa75fdf19644f125224b5c74595
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-06 22:08:19 +0530

    Time: 35 ms (80.95%) | Memory: 69.5 MB (75.06%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int n = nums.size();
-        int maxi = 0;
-        int minIcre = 0;
+        sort(nums.begin(),nums.end());
+        int cnt = 0;
 
-        for(int val : nums){
-            maxi = max(maxi,val);
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] <= nums[i-1]){
+                int needed = nums[i-1]+1;
+                cnt += needed-nums[i];
+                nums[i] = needed;
+            }
         }
-
-        vector<int> freq(n+maxi,0);
-
-        for(int val : nums){
-            freq[val]++;
-        }
-
-        for(int i = 0 ; i < freq.size(); i++){
-            if(freq[i] <= 1) continue;
-
-            int dup = freq[i]-1;
-            freq[i+1] += dup;
-            freq[i] = 1;
-
-            minIcre += dup;
-        }
-        return minIcre;
+        return cnt;
     }
 };
