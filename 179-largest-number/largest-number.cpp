5e0b0f17fdfe45bc3394eee32aa5b29e9ecc710b
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-10 11:32:26 +0530

    Time: 3 ms (75.52%) | Memory: 15.1 MB (96.72%) - LeetSync

---
+++ 179-largest-number/largest-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        auto lambda = [](int &a , int &b){
+            string s1 = to_string(a);
+            string s2 = to_string(b);
+
+            if(s1+s2 > s2+s1){
+                return true;
+            }
+            return false;
+        };
+        sort(begin(nums),end(nums),lambda);
+
+        if(nums[0] == 0) return "0";
+        string res = "";
+        for(int &x : nums){
+            res += to_string(x);
+        }
+
+        return res;
+    }
+};
