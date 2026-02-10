4dab29c04d5fedbef7921f83934331446cc4462f
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-10 09:45:58 +0530

    Time: 1893 ms (64.36%) | Memory: 530.4 MB (41.51%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -5,20 +5,19 @@ public:
         int maxi = 0;
 
         for(int i = 0 ; i < n ; i++){
-            unordered_set<int> evenSet;
-            unordered_set<int> oddSet;
+            unordered_set<int>evens;
+            unordered_set<int>odd;
 
             for(int j = i ; j < n ; j++){
-                if( nums[j] % 2 == 0){
-                    evenSet.insert(nums[j]);
+                if(nums[j] % 2 == 0){
+                    evens.insert(nums[j]);
                 }
                 else{
-                    oddSet.insert(nums[j]);
+                    odd.insert(nums[j]);
                 }
-
-                if(evenSet.size() == oddSet.size()){
-                    maxi = max(maxi , j-i+1);
-                }
+                if(evens.size() == odd.size()){
+                maxi = max(maxi , j-i+1);
+            }
             }
         }
         return maxi;
