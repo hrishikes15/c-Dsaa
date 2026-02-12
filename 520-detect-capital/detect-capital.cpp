f4fee901717f2a94496b3eb294e4a588cfa21804
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-12 20:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (15.77%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int capitalCnt = 0;
-
+        int capitalCnt = 0; 
         for(int i = 0 ; i < word.size() ; i++){
             if(isupper(word[i])) capitalCnt++;
         }
-
-        if(capitalCnt == word.size()) return true;
-        
-        if(capitalCnt == 0) return true;
-
-        if(capitalCnt == 1 && isupper(word[0])) return true;
+        if(capitalCnt == word.size() || (capitalCnt == 0) ||(capitalCnt == 1 && isupper(word[0])) ) return true;
 
         return false;
     }
