828ed6bac7cf3d4d869eeb8374beed2c9d8d42e1
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-12 13:01:21 +0530

    Time: 19 ms (7.89%) | Memory: 19.5 MB (7.11%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        
-        auto lambda = [](char ch){
+        return any_of(begin(s),end(s),[](char ch){
             return  string("aeiou").find(ch) != string :: npos;
-        };
-        
-        return any_of(begin(s),end(s),lambda);
+        });
     }
 };
