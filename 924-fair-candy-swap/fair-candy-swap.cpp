a03f798d4e6de4347e7fbdfc7f686a499cd0c829
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-02-24 19:15:49 +0530

    Time: 17 ms (86.04%) | Memory: 43 MB (71.98%) - LeetSync

--- 924-fair-candy-swap/fair-candy-swap.cpp
+++ 924-fair-candy-swap/fair-candy-swap.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& a, vector<int>& b) {
-        int sum1 = reduce(begin(a),end(a));
-        int sum2 = reduce(begin(b),end(b));
+        int s1 = accumulate(begin(a),end(a),0);
+        int s2 = accumulate(begin(b),end(b),0);
 
-        int diff = (sum1 - sum2) / 2;
-
+        int diff = (s1-s2)/2;
         sort(begin(a),end(a));
         sort(begin(b),end(b));
 
         int i = 0 , j = 0;
-        while(i < size(a) && j < size(b)){
+
+        while(i < a.size() && j < b.size()){
             int curr = a[i] - b[j];
 
-            if(curr == diff) return {a[i],b[j]};
+            if(curr == diff) return{a[i],b[j]};
 
             if(curr < diff){
                 i++;
