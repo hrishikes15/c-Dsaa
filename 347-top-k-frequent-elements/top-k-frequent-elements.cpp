cc15f4fa7b6ca0a0558c60a7310cb9a29ecd9887
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-08 11:10:40 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (62.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,28 +1,21 @@
 class Solution {
-    typedef  pair<int,int> P;
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>mapp;
         for(int x : nums){
             mapp[x]++;
         }
+        vector<pair<int,int>> vec(mapp.begin(),mapp.end());
 
-        priority_queue<P ,  vector<P> , greater<P>> pq;
+        auto hrishi = [&](pair<int,int>& a , pair<int,int>&b){
+            return a.second > b.second;
+        };
 
-        for(auto &it : mapp){
-            int val = it.first;
-            int freq = it.second;
+        sort(vec.begin(),vec.end(),hrishi);
 
-            pq.push({freq,val});
-
-            if(pq.size() > k){
-                pq.pop();
-            }
-        }
-        vector<int> ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
+        vector<int>ans;
+        for(int i = 0 ; i < k ; i++){
+            ans.push_back(vec[i].first);
         }
         return ans;
     }
