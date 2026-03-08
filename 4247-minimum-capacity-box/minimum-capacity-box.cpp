4e87c875a31158b7502d952589d2f1ad59f22a5f
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-08 23:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (40.81%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.cpp
+++ 4247-minimum-capacity-box/minimum-capacity-box.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& capacity, int itemSize) {
-        int n = capacity.size();
-        
-        vector<pair<int,int>> v;
-        for(int i = 0; i < n; i++){
-            v.push_back({capacity[i], i});
-        }
+        int minCap = INT_MAX;
+        int ans = -1;
 
-        sort(v.begin(), v.end());
-
-        auto it = lower_bound(v.begin(), v.end(), make_pair(itemSize, -1));
-
-        if(it == v.end()) return -1;
-
-        int minCap = it->first;
-        int ans = it->second;
-        while(it != v.end() && it->first == minCap){
-            ans = min(ans, it->second);
-            it++;
+        for(int i = 0; i < capacity.size(); i++){
+            if(capacity[i] >= itemSize && capacity[i] < minCap){
+                minCap = capacity[i];
+                ans = i;
+            }
         }
 
         return ans;
