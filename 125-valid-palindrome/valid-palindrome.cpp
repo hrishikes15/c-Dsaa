d023d51da5110231233f33418a66471c738b0451
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-13 10:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (36.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,26 @@
 class Solution {
+    bool valid(char &ch){
+       char  c = tolower(ch);
+        if(c >= 'a' && c <= 'z') return true;
+        if(c >= '0' && c <= '9')  return true;
+        return false;
+    }
 public:
     bool isPalindrome(string s) {
         int n = s.size();
         int i = 0 , j = n-1;
-
         while(i <= j){
-            if(!isalnum(s[i])) i++;
-            else if(!isalnum(s[j]))j--;
-            else{
-                if(tolower(s[i]) != tolower(s[j])) return false;
-
-                i++;j--;
+            if(!valid(s[i])){
+                i++;
+                continue;
             }
+            if(!valid(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])) return false;
+
+            i++;j--;
         }
         return true;
     }
