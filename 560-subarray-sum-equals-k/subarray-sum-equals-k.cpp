6a65f63fa7ce2b659758a983c817ec488bb4f8fc
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-13 10:06:23 +0530

    Time: 30 ms (96.56%) | Memory: 45.3 MB (68.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,14 +3,17 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> mapp;
         mapp[0] = 1;
-        int preFix = 0 , cnt = 0;
+        int currSum = 0 , cnt = 0;
         for(int i = 0 ; i < nums.size() ; i++){
-            preFix += nums[i];
-            int remove = preFix - k;
-            cnt += mapp[remove];
-            mapp[preFix]++;
+            currSum += nums[i];
+            int rem = currSum - k;
+
+            if(mapp.find(rem) != mapp.end()){
+                cnt += mapp[rem];
+            }
+            // cnt += mapp[rem];
+            mapp[currSum]++;
         }
         return cnt;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
