917e7d57672c50e65093bcc0eb5e548c0802c935
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-13 14:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (46.80%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size();
-        int idx = 0 , i = 0;
+        int n  = chars.size();
+        int i = 0 , idx = 0;
 
         while(i < n){
-            char curr_ch = chars[i];
+            char curr_char = chars[i];
             int cnt = 0;
-
-            while(i < n && chars[i] == curr_ch){      //counting
+            while(  i < n && curr_char == chars[i]){
                 cnt++;i++;
             }
 
-            chars[idx] = curr_ch; idx++;                    //assigning
+            chars[idx] = curr_char;
+            idx++;
             if(cnt > 1){
                 string str = to_string(cnt);
-                for(char &ch: str){
+                for(char &ch : str){
                     chars[idx] = ch;
                     idx++;
                 }
             }
+
         }
         return idx;
     }
