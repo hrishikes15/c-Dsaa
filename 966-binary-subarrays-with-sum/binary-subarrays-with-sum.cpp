74c4b3a7241e3d3a1e167ddf6d23bc8cd164192d
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-15 10:17:09 +0530

    Time: 23 ms (18.21%) | Memory: 38.7 MB (26.82%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return func(nums, goal) - func(nums, goal - 1);
-    }
-
-    int func(vector<int>& nums, int goal) {
-        if (goal < 0) return 0;
-
-        int count = 0;
-        int i = 0, j = 0;
-        int n = nums.size();
+        unordered_map<int,int>mapp;
+        mapp[0] = 1;
+        int cnt = 0;
         int sum = 0;
+        for(int i = 0; i < nums.size() ; i++){
+            sum += nums[i];
+            int rem = sum - goal;
 
-        while (j < n) {
-            sum += nums[j];
-
-            while (i <= j && sum > goal) {
-                sum -= nums[i];
-                i++;
+            if(mapp.find(rem) != mapp.end()){
+                cnt += mapp[rem];
             }
-
-            count += (j - i + 1);
-
-            j++;
+            mapp[sum]++;
         }
-
-        return count;
+        return cnt;
     }
 };
