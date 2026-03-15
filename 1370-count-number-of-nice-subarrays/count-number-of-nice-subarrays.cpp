57324d395b01c864753c6b66e66c8b7601196972
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-15 18:21:59 +0530

    Time: 76 ms (15.21%) | Memory: 94.2 MB (17.81%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return  func ( nums , k ) - func( nums , k-1);
-    }
-
-    int func(vector<int>& nums, int goal) {
-        if (goal < 0) return 0;
-
-        int count = 0;
-        int i = 0, j = 0;
+        int i=0 , j=0;
+        unordered_map<int,int>mapp;
         int n = nums.size();
         int sum = 0;
+        int cnt = 0;
+        mapp[0] = 1;
 
-        while (j < n) {
-            sum += nums[j] % 2;
+        for(int i = 0 ; i<n ; i++){
+            sum += nums[i]%2;
 
-
-            while (i <= j && sum > goal) {
-                sum -= nums[i] % 2;
-                i++;
+            int rem = sum - k;
+            if(mapp.find(rem) != mapp.end()){
+                cnt+=mapp[rem];
             }
-
-           
-            count += (j - i + 1);
-
-            j++;
+            mapp[sum]++;
         }
- 
-        return count;
+        return cnt;
     }
 };
