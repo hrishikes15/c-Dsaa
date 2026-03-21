aecb88224b85c5cfb96642c2e8233c5dd18b4e42
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-21 19:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.19%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,45 @@
 class Solution {
-    int solve(int i , int j , vector<vector<int>>& grid , vector<vector<int>>&dp){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(i >= m || j >= n || grid[i][j] == 1){
-            return 0;
-        }
-        if(i == m-1 && j == n-1){
-            return 1;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i , j+1 , grid,dp);
-        int down = solve(i+1 , j , grid,dp);
-
-        return dp[i][j] =  right + down;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
-        return solve(0,0,grid , dp);
+        vector<vector<int>> dp(m , vector<int>(n ,0));
+
+        // first row
+        for(int col = 0 ; col < n ; col++){
+            if(grid[0][col] == 1){
+                dp[0][col] = 0;
+            }
+            else if(col == 0){
+                dp[0][col] = 1;
+            }
+            else{
+                dp[0][col] = dp[0][col-1];
+            }
+        }
+        // first column
+        for(int row = 0 ; row < m ; row++){
+            if(grid[row][0] == 1){
+                dp[row][0] = 0;
+            }
+            else if(row == 0){
+                dp[row][0] = 1;
+            }
+            else{
+                dp[row][0] = dp[row-1][0];
+            }
+        }
+        // rest
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ;  j < n ; j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
