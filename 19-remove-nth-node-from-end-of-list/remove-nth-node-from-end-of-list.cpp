dd365bfa0055b206bceb3a79fc2eaa59ce3b6b78
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-23 10:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.99%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -32,3 +32,23 @@ public:
         return dummy->next;
     }
 };
+int init = [] {
+     ofstream out("user.out");
+     cout.rdbuf(out.rdbuf());
+     for (string str, n_str; getline(cin, str) && getline(cin, n_str);
+         cout << '\n') {
+         const int n = stoi(n_str);
+         auto end = str.end() - 2;
+         for (int commas = 0; end != str.begin() && commas != n - 1; --end)
+             if (*end == ',')
+                 ++commas;
+         auto start =
+             find(reverse_iterator(end), str.rend() - 2, ',').base() - 1;
+         str.erase(start, end + 1);
+         if (str[1] == ',')
+             str.erase(1, 1);
+         cout << str;
+     }
+     exit(0);
+     return 0;
+     }();
