fe393cc705fa1352d4b5500fd4445fd615a642bb
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-23 10:00:04 +0530

    Time: 4 ms (8.63%) | Memory: 17.1 MB (5.64%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,33 +1,24 @@
 class Solution {
-public:
-   void solve(vector<int> ip , vector<int> op , vector<vector<int>> &ans){
-       
-        
-        if(ip.size() == 0 ){
+    void solve(vector<int>ip , vector<vector<int>>&ans , vector<int>op){
+        if(ip.size() == 0){
             ans.push_back(op);
             return;
         }
-
-        vector<int> op1 = op;
+        vector<int>op1 = op;
+        vector<int>op2 = op;
         
-        vector<int> op2 = op;
-
         op2.push_back(ip[0]);
         ip.erase(ip.begin());
 
-        solve(ip , op1  , ans);
+        solve(ip,ans,op1);
+        solve(ip,ans,op2);
 
-        solve(ip , op2  , ans);
-
-        
-        
-   }
+    }
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> op;
-        solve(nums, op , ans);
-
+        vector<vector<int>>ans;
+        vector<int>op;
+        solve(nums,ans,op);
         return ans;
-        
     }
 };
