362cad90f0b5908366d16509e8ce8de3064c8195
hrishikes15 <145208329+hrishikes15@users.noreply.github.com>
2026-03-29 10:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (49.03%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        vector<int> even(26, 0), odd(26, 0);
-
-        for(int i = 0; i < s1.size(); i++) {
-            if(i % 2 == 0) {
-                even[s1[i] - 'a']++;
-                even[s2[i] - 'a']--;
-            } else {
-                odd[s1[i] - 'a']++;
-                odd[s2[i] - 'a']--;
+        for(int i = 0; i < 2; i++){
+            if(s1[i] != s2[i]){
+                swap(s1[i], s1[i+2]);
             }
         }
-
-        return even == vector<int>(26, 0) && odd == vector<int>(26, 0);
+        return s1 == s2;
     }
 };
