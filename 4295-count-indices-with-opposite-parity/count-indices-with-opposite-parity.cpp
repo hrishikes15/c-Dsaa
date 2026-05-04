2aa9fd9cd3b759bc4466afe583bcc7e5a5282378
Hrishikesh Yadav G <145208329+hrishikes15@users.noreply.github.com>
2026-05-04 18:48:55 +0530

    Time: 7 ms (0.00%) | Memory: 33.1 MB (0.00%) - LeetSync

---
+++ 4295-count-indices-with-opposite-parity/count-indices-with-opposite-parity.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> countOppositeParity(vector<int>& nums) {
+        vector<int>ans;
+        int n = nums.size();
+
+        for(int i =0 ; i < n ; i++){
+            int cnt = 0;
+            for(int j = i+1 ; j < n ; j++){
+                if(nums[i] % 2 == 0){
+                    if(nums[j] % 2 != 0){
+                        cnt++;
+                    }
+                }
+                else if(nums[i]%2 != 0){
+                    if(nums[j] % 2 == 0){
+                        cnt++;
+                    }
+                }
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
